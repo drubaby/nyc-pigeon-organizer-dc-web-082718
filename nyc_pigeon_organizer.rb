6966f40ec452514b363df044e0c733b18fa0f5c0
drubaby <peryton6@gmail.com>
2018-08-20 11:26:43 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, pigeon_facts| #attribute = color, gender, lives
+    pigeon_facts.each do |fact, name|
+      name.each do |pigeon|
+
+      if pigeon_list.has_key?(pigeon) == false #statement stops loop from overwriting
+         pigeon_list[pigeon] = {} #"Theo"=>{}, "Peter Jr."=>{}, etc WORKS
+      end
+
+      if pigeon_list[pigeon].key?(attribute) == false #statement stops loop from overwriting
+         pigeon_list[pigeon][attribute] = []
+      end
+
+       pigeon_list[pigeon][attribute] << fact.to_s
+       
+     end
+   end
+ end
+pigeon_list
 end
